a9e92c1fdee7a7444d0b7bad7fe46843c83f23e4
Brian Ginsburg <brian@brianginsburg.com>
2017-05-28 11:16:40 -0700

    Add instructions and remove debug elements

--- src/View.elm
+++ src/View.elm
@@ -15,9 +15,7 @@ view model =
         , showRow model.fifth
         , showRow model.third
         , showRow model.root
-        , controls
-        , debugScore model.score
-        , debugClock model.clock
+        , instructions model.clock
         ]
 
 
@@ -37,6 +35,28 @@ controls =
             ]
         , div [ class "column small-1" ]
             [ a [ class "expanded hollow button fa fa-play", onClick Play ] []
+            ]
+        ]
+
+
+instructions : Int -> Html Msg
+instructions clock =
+    div [ class "row align-center" ]
+        [ div [ class "column small-6" ]
+            [ p []
+                [ text "Minima is a playground for experimenting with minimalist musical patterns. Press "
+                , a [ class "control fa fa-play", onClick Play ] []
+                , text "to start the music and"
+                , a [ class "control fa fa-pause", onClick Pause ] []
+                , text "to stop it."
+                , text " Select patterns with the "
+                , a [ class "control secondary fa fa-chevron-left" ] []
+                , text "and  "
+                , a [ class "control secondary fa fa-chevron-right" ] []
+                , text "buttons. Minima has four beats, and the current beat is "
+                , span [ class "clock" ] [ text (showClock clock) ]
+                , text ". Have fun and I hope you enjoy!"
+                ]
             ]
         ]
 
@@ -84,38 +104,42 @@ rotateVoice voice direction =
         ]
 
 
-
--- DEBUG
-
-
-debugScore : Score -> Html Msg
-debugScore score =
-    div [ class "row" ]
-        [ div [ class "columns" ]
-            [ h3 [] [ text (showScore score) ] ]
-        ]
-
-
-debugClock : Int -> Html Msg
-debugClock clock =
-    div [ class "row" ]
-        [ div [ class "columns" ]
-            [ h3 [] [ text (showClock clock) ] ]
-        ]
-
-
-showScore : Score -> String
-showScore score =
-    List.map .frequency score
-        |> List.map toString
-        |> String.join ", "
-
-
 showClock : Int -> String
 showClock clock =
-    case (clock < 2) of
-        True ->
-            toString 4
+    case clock of
+        0 ->
+            "0"
 
-        False ->
+        1 ->
+            "4"
+
+        _ ->
             toString (clock - 1)
+
+
+
+-- DEBUG
+-- debugScore : Score -> Html Msg
+-- debugScore score =
+--     div [ class "row" ]
+--         [ div [ class "columns" ]
+--             [ h3 [] [ text (showScore score) ] ]
+--         ]
+-- debugClock : Int -> Html Msg
+-- debugClock clock =
+--     div [ class "row" ]
+--         [ div [ class "columns" ]
+--             [ h3 [] [ text (showClock clock) ] ]
+--         ]
+-- showScore : Score -> String
+-- showScore score =
+--     List.map .frequency score
+--         |> List.map toString
+--         |> String.join ", "
+-- showClock : Int -> String
+-- showClock clock =
+--     case (clock < 2) of
+--         True ->
+--             toString 4
+--         False ->
+--             toString (clock - 1)
